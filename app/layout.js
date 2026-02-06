a221957b60fc033fd220ec6c324fd0a143a105ad
susmailspam-design <susmail.spam@gmail.com>
2026-02-06 09:42:22 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,9 @@
-export const metadata = {
-  title: 'The Codex',
-  description: 'My App',
-}
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
+      </head>
       <body>{children}</body>
     </html>
   )
